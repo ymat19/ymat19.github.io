d9dadaa3526e58c0aa5525e237f39d71493b35f2
mtbyk <mtbyk@mtbyk-air.local>
2024-12-17 04:58:40 +0900

    時計を配置

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Center, VStack, Text } from "@chakra-ui/react";
 
 export function Timer() {
-    const [time, setTime] = useState(0);
-    return <>timer</>
+  const now = new Date();
+  const [time, setTime] = useState(now);
+
+  const task = setInterval(() => {
+    setTime(new Date());
+  }, 1000);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(task);
+    };
+  });
+
+  return (
+    <Center h="100vh">
+      <VStack
+        w="80%"
+        h="80%"
+        padding={"40px 80px"}
+        justifyContent="center"
+        border="1px solid white"
+        borderRadius="10px"
+        gap={"20px"}
+      >
+        <Text fontSize="6xl" fontWeight="bold">
+            {time.toLocaleTimeString()}
+        </Text>
+      </VStack>
+    </Center>
+  );
 }
