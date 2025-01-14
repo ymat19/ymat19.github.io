aa4b03e16ee17b642c05914f024efe58e334e244
mtbyk <mtbyk@mtbyk-air.local>
2025-01-15 06:06:11 +0900

    2回に一回しか入力できない

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,3 +1,48 @@
+import { Text, Button, HStack, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
 export function Calculator() {
-    return (<></>);
+  const buttonsLabels: string[][] = [
+    ["7", "8", "9", "÷"],
+    ["4", "5", "6", "×"],
+    ["1", "2", "3", "-"],
+    ["0", "AC", "=", "+"],
+  ];
+
+  const [expression, setExpression] = useState<string>("");
+  const onClickDefault = (event: React.MouseEvent<HTMLButtonElement>) => setExpression((value) => value + (event.currentTarget?.textContent || ""));
+
+  return (
+    <>
+      <Text>Calculator</Text>
+      <Input value={expression} placeholder="" />
+      {buttonsLabels.map((labels) => (
+        <HStack key={labels.join()} gap={"4px"} margin={"4px"}>
+          {labels.map((label) => (
+            <CalcButton
+              key={label}
+              label={label}
+              onClick={label === "AC" ? (_: React.MouseEvent<HTMLButtonElement>) => setExpression("") : onClickDefault}
+            />
+          ))}
+          <br />
+        </HStack>
+      ))}
+    </>
+  );
 }
+
+interface CalcButtonProps {
+  label: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const CalcButton: React.FC<CalcButtonProps> = ({
+  label,
+  onClick,
+}) => {
+  return (
+    <Button minWidth={"64px"} onClick={onClick}>
+      {label}
+    </Button>
+  );
+};
